6a57f5ebcc6bb6b63b09871d42e400929411b49f
Venipa <admin@venipa.net>
2018-09-19 19:15:00 +0200

    token preg

--- app/Http/Controllers/ShortenerController.php
+++ app/Http/Controllers/ShortenerController.php
@@ -92,6 +92,7 @@ class ShortenerController extends Controller
         ]);
     }
     public function getLink(Request $r, $token, $slug = null, $ignorePass = false) {
+        $token = preg_replace('/\..+$/', '',$token);
         $v = Validator::make($r->merge(['token' => $token])->all(), [
             'token' => 'required|exists:links,token'
         ]);
