77780f82af4ca7b6ed60c98177468048fae7e3e2
Venipa <admin@venipa.net>
2018-11-03 22:49:09 +0100

    audio fix

--- app/VideoStream.php
+++ app/VideoStream.php
@@ -38,7 +38,7 @@ class VideoStream
     private function setHeader()
     {
         ob_get_clean();
-        header("Content-Type: video/mp4");
+        header("Content-Type: $this->type");
         header("Cache-Control: max-age=2592000, public");
         header("Expires: ".gmdate('D, d M Y H:i:s', time()+2592000) . ' GMT');
         header("Last-Modified: ".gmdate('D, d M Y H:i:s', @filemtime($this->path)) . ' GMT' );
