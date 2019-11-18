00ed9c153f378d5d09524120dbf11464e89258e6
Venipa <admin@venipa.net>
2019-11-18 21:02:21 +0100

    fixed login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,27 +18,33 @@ use FFMpeg\FFMpeg;
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('welcome');
     }
-    public function login(Request $r) {
-        if(!$r->isMethod('POST')) return view('auth.login');
+    public function login(Request $r)
+    {
+        if (!$r->isMethod('POST')) return view('auth.login');
         $v = Validator::make($r->all(), [
             'email' => 'required|email|exists:users,email',
             'password' => 'required|min:5|max:30'
         ]);
-        if($v->fails()) return redirect()->back()->withErrors($v->errors());
+        if ($v->fails()) return redirect()->back()->withErrors($v->errors());
         $user = User::where('email', $r->input('email'))->first();
-        if(Hash::check($r->input('password'), $user->password)) {
-            Auth::login($user);
+
+        if (auth()->attempt([
+            'email' => $r->input('email'),
+            'password' => $r->input('password')
+        ], true)) {
             return redirect()->route('files');
         } else {
             $v->errors()->add('password', 'Email or Password does not seem to be correct');
             return redirect()->back()->withErrors($v->errors());
         }
     }
-    public function files() {
-        $user = Auth::user()->first();
+    public function files()
+    {
+        $user = auth()->user();
         $files = $user->files()->latest()->paginate(25);
         return view('files')->with(['user' => $user, 'files' => $files]);
     }
