f79a675cf300d63546fb150fa524a50c6d7e78d1
Venipa <admin@venipa.net>
2019-12-01 20:30:44 +0100

    updated dashboard view route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,7 +43,7 @@ class HomeController extends Controller
     }
     public function dashboard() {
         // dd(auth()->user()->withRoleSettings()->first());
-        return view('dashboard\index');
+        return view('dashboard.index');
     }
     public function files()
     {
