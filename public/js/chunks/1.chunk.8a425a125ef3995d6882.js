09ca4f349c0931ee19b7ee6015865da4ef95ab63
Venipa <admin@venipa.net>
2019-12-06 19:04:53 +0100

    showing uploaded file in upload dialog

---
+++ public/js/chunks/1.chunk.8a425a125ef3995d6882.js
@@ -0,0 +1,194 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./node_modules/vue-loader/lib/index.js?!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib??vue-loader-options!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "ContentCopyIcon",
+  props: {
+    title: {
+      type: String,
+      default: "Content Copy icon"
+    },
+    decorative: {
+      type: Boolean,
+      default: false
+    },
+    fillColor: {
+      type: String,
+      default: "currentColor"
+    },
+    size: {
+      type: Number,
+      default: 24
+    }
+  }
+});
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true&":
+/*!*********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true& ***!
+  \*********************************************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function(_h, _vm) {
+  var _c = _vm._c
+  return _c(
+    "span",
+    _vm._g(
+      _vm._b(
+        {
+          staticClass: "material-design-icon content-copy-icon",
+          class: [_vm.data.class, _vm.data.staticClass],
+          attrs: {
+            "aria-hidden": _vm.props.decorative,
+            "aria-label": _vm.props.title,
+            role: "img"
+          }
+        },
+        "span",
+        _vm.data.attrs,
+        false
+      ),
+      _vm.listeners
+    ),
+    [
+      _c(
+        "svg",
+        {
+          staticClass: "material-design-icon__svg",
+          attrs: {
+            fill: _vm.props.fillColor,
+            width: _vm.props.size,
+            height: _vm.props.size,
+            viewBox: "0 0 24 24"
+          }
+        },
+        [
+          _c(
+            "path",
+            {
+              attrs: {
+                d:
+                  "M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
+              }
+            },
+            [_c("title", [_vm._v(_vm._s(_vm.props.title))])]
+          )
+        ]
+      )
+    ]
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-material-design-icons/ContentCopy.vue":
+/*!****************************************************************!*\
+  !*** ./node_modules/vue-material-design-icons/ContentCopy.vue ***!
+  \****************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ContentCopy.vue?vue&type=template&id=64e26d12&functional=true& */ "./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true&");
+/* harmony import */ var _ContentCopy_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ContentCopy.vue?vue&type=script&lang=js& */ "./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _ContentCopy_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  true,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "node_modules/vue-material-design-icons/ContentCopy.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js&":
+/*!*****************************************************************************************!*\
+  !*** ./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js& ***!
+  \*****************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _vue_loader_lib_index_js_vue_loader_options_ContentCopy_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../vue-loader/lib??vue-loader-options!./ContentCopy.vue?vue&type=script&lang=js& */ "./node_modules/vue-loader/lib/index.js?!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_vue_loader_lib_index_js_vue_loader_options_ContentCopy_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true&":
+/*!***************************************************************************************************************!*\
+  !*** ./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true& ***!
+  \***************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _vue_loader_lib_loaders_templateLoader_js_vue_loader_options_vue_loader_lib_index_js_vue_loader_options_ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../vue-loader/lib??vue-loader-options!./ContentCopy.vue?vue&type=template&id=64e26d12&functional=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./node_modules/vue-material-design-icons/ContentCopy.vue?vue&type=template&id=64e26d12&functional=true&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _vue_loader_lib_loaders_templateLoader_js_vue_loader_options_vue_loader_lib_index_js_vue_loader_options_ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _vue_loader_lib_loaders_templateLoader_js_vue_loader_options_vue_loader_lib_index_js_vue_loader_options_ContentCopy_vue_vue_type_template_id_64e26d12_functional_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
+
+/***/ })
+
+}]);
--- resources/assets/js/components/Dialogs/UploadDialogComponent.vue
+++ resources/assets/js/components/Dialogs/UploadDialogComponent.vue
@@ -1,42 +1,74 @@
 <template>
-  <b-modal
-    centered
-    ref="showUpload"
-    title="Upload"
-    size="xl"
-    @hide="onReloadAll"
-    :no-close-on-esc="isLoading"
-    :no-close-on-backdrop="isLoading"
-  >
+  <b-modal centered
+           ref="showUpload"
+           title="Upload"
+           size="xl"
+           @hide="onReloadAll"
+           :no-close-on-esc="isLoading"
+           :no-close-on-backdrop="isLoading">
     <template v-slot:modal-footer>
       <div class="d-flex flex-column">
-        <b-button
-          class="d-flex flex-row"
-          variant="dark"
-          @click.prevent="onUpload"
-          :disabled="isLoading"
-        >
+        <b-button class="d-flex flex-row"
+                  variant="dark"
+                  @click.prevent="onUpload"
+                  :disabled="isLoading">
           <span>Upload</span>
-          <app-spinner v-if="isLoading" class="ml-2" :size="8" sizeUnit="px" color="#ffffff" />
+          <app-spinner v-if="isLoading"
+                       class="ml-2"
+                       :size="8"
+                       sizeUnit="px"
+                       color="#ffffff" />
         </b-button>
       </div>
     </template>
-    <div class="d-flex flex-column drop-wrapper">
-      <file-drop
-        name="file"
-        ref="uploadDrop"
-        label-idle="Drop files here..."
-        allow-multiple="true"
-        :server="`/api/v1/upload`"
-        :instantUpload="false"
-        method="post"
-        :max-files="25"
-        :allowRevert="false"
-        v-bind:files="pond.files"
-        @processfile="onFileComplete"
-        @init="handleFilePondInit"
-      />
+    <b-tabs nav-class="mb-3">
+      <b-tab title="Upload"
+             active>
+        <div class="d-flex flex-column drop-wrapper">
+          <file-drop name="file"
+                     ref="uploadDrop"
+                     label-idle="Drop files here..."
+                     allow-multiple="true"
+                     :server="`/api/v1/upload`"
+                     :instantUpload="false"
+                     method="post"
+                     :max-files="25"
+                     :allowRevert="false"
+                     v-bind:files="pond.files"
+                     @init="handleFilePondInit" />
+        </div>
+      </b-tab>
+      <b-tab v-if="pond.createdFiles && pond.createdFiles.length > 0"
+             title="Uploaded Files">
+        <div class="mb-4 mx-2 d-flex flex-row justify-content-end">
+          <b-checkbox v-model="showRaw"
+                      :unchecked-value="false"
+                      :checked="true">
+            Raw Links
+          </b-checkbox>
+        </div>
+        <pre v-if="showRaw"
+             class="pre-links pre-scrollable border p-2"><template v-for="file in pond.createdFiles">{{file.server.url}}
+</template></pre>
+        <template v-else v-for="file in pond.createdFiles" class="d-flex flex-column justify-content-center">
+  <div class="card p-2 d-flex flex-row align-items-center justify-content-start mb-3"
+       :key="file.info.id"
+       no-body>
+    <div class="flex-grow-1">
+      <strong class="border-right pr-2 mr-2">{{filesize(file.info.fileSize || 0)}}</strong>
+      <span>{{file.info.filename}}</span>
     </div>
+    <b-button @click="copyLink(file.server.url)"
+              v-b-tooltip.hover
+              title="Copy Link"
+              variant="link"
+              class="btn-icon text-primary">
+      <copy-icon :size="20"></copy-icon>
+    </b-button>
+  </div>
+</template>
+      </b-tab>
+    </b-tabs>
   </b-modal>
 </template>
 
@@ -45,10 +77,12 @@ import vueFilePond, { FileStatus } from "vue-filepond";
 import { PulseLoader } from "@saeris/vue-spinners";
 import { axios } from "../../app";
 import { user } from "../../api";
+import fileSize from "filesize";
 const FilePond = vueFilePond();
 
 const defaultOptions = () => {
   return {
+    showRaw: false,
     dropzoneOptions: {
       url: window.location.origin + `/api/v1/upload`,
       headers: axios.defaults.headers,
@@ -63,6 +97,7 @@ const defaultOptions = () => {
     },
     pond: {
       files: [],
+      createdFiles: [],
       options: {
         instantUpload: false
       }
@@ -73,18 +108,31 @@ const defaultOptions = () => {
 export default {
   components: {
     "app-spinner": PulseLoader,
-    "file-drop": FilePond
+    "file-drop": FilePond,
+    "menu-icon": () => import("vue-material-design-icons/DotsVertical"),
+    "copy-icon": () => import("vue-material-design-icons/ContentCopy")
   },
   data() {
     return defaultOptions();
   },
   created() {},
   methods: {
+    filesize(arg) {
+      return fileSize(arg);
+    },
     open() {
       this.$refs["showUpload"].show();
     },
     close() {
       this.$refs["showUpload"].hide();
+    },
+    showToast(title, msg, type = "danger") {
+      this.$bvToast.toast(msg, {
+        title: title,
+        variant: type || "default",
+        autoHideDelay: 5000,
+        appendToast: true
+      });
     },
     onUpload(ev) {
       const drop = this.$refs.uploadDrop;
@@ -94,7 +142,24 @@ export default {
         return;
       }
       this.isLoading = true;
-      drop.processFiles();
+      drop.processFiles().then(files => {
+        this.isLoading = false;
+        if (files && files.length > 0) {
+          this.pond.createdFiles.push(
+            ...files.map(x => {
+              return { info: x, server: JSON.parse(x.serverId) };
+            })
+          );
+        }
+      });
+    },
+    copyLink(url) {
+      this.$clipboard(url);
+      this.showToast(
+        "Copy Link",
+        `Link has been copied to clipboard`,
+        "success"
+      );
     },
     handleFilePondInit(ev) {
       console.log(ev);
@@ -105,15 +170,6 @@ export default {
           .length > 0
       ) {
         this.$emit("on-upload-complete", this.$refs.uploadDrop);
-      }
-    },
-    onFileComplete() {
-      if (
-        this.$refs.uploadDrop
-          .getFiles()
-          .filter(x => [2, 3, 9, 7].includes(x.status)).length === 0
-      ) {
-        this.isLoading = false;
       }
     },
     onComplete() {
@@ -144,5 +200,8 @@ export default {
   .filepond--panel-bottom {
     border-radius: 0 !important;
   }
+}
+.pre-links {
+  background-color: #fafafa;
 }
 </style>
